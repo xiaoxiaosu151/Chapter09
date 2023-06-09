73fb9c766b6e62f4d709cb619609a0c4390eda97
xiaoxiaosu151 <282612325@qq.com>
2023-06-09 16:57:32 +0800

    修改打印日志

--- src/main/java/cn/lzy/ChapterApplication.java
+++ src/main/java/cn/lzy/ChapterApplication.java
@@ -3,6 +3,6 @@ package cn.lzy;
 public class ChapterApplication {
 
     public static void main(String[] args) {
-        System.out.println("大家，下午好！！");
+        System.out.println("大家，下午好  springboot！！");
     }
 }
